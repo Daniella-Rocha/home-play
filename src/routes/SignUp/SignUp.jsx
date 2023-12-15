55bb6c4efb3927f30beb3df6ae68690c0fa3ed32
Daniella-Rocha <daniella.fp.dr@gmail.com>
2023-12-15 09:02:57 -0300

    Criado validação de formulário de cadastro de usuário

--- src/routes/SignUp/SignUp.jsx
+++ src/routes/SignUp/SignUp.jsx
@@ -1,36 +1,118 @@
+import { useForm } from "react-hook-form";
+
+import useCadastrarUsuario from '../../hooks/useCadastrarUsuario';
+
 import styles from './SignUp.module.css';
 
-import ButtonSubmit from "../../components/ButtonSubmit/ButtonSubmit"
-import FieldText from "../../components/FieldText/FieldText"
+import Form from 'react-bootstrap/Form';
+
+import ButtonSubmit from "../../components/ButtonSubmit/ButtonSubmit";
+
+import './SignUp.css';
 
 const SignUp = () => {
+  const { signUp, setSignUp, signUpUser } = useCadastrarUsuario();
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = async (data) => {
+    setSignUp({
+      name: data.name,
+      email: data.email,
+      password: data.password
+    });
+    signUpUser();
+  };
+
   return (
     <div className={styles.signup}>
       <div className={styles.box_signup}>
         <h1>Criar uma Conta</h1>
-        <form>
-          <FieldText
-            id={'fullname'}
-            type={'text'}
-            placeholder={'*Nome Completo'}
-            obrigatorio
-          />
-          <FieldText
-            id={'email'}
-            type={'text'}
-            placeholder={'*E-mail'}
-            obrigatorio
-          />
-          <FieldText
-            id={'password'}
-            type={'password'}
-            placeholder={'Senha'}
-            obrigatorio
-          />
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group
+            className="mb-3"
+            controlId="name"
+          >
+            <Form.Label>Nome</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Seu Nome Aqui"
+              {...register("name",
+                {
+                  required: "Campo nome é obrigatório",
+                  maxLength: {
+                    value: 30,
+                    message: "Este campo deve ter no máximo 30 caracteres"
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z\s]+$/gi,
+                    message: "Este campo deve conter apenas letras"
+                  }
+                }
+              )}
+            />
+            <span className="error">{errors.name?.message}</span>
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            controlId="email"
+          >
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              {...register("email",
+                {
+                  required: 'Este campo é obrigatório.',
+                  minLength: {
+                    value: 10,
+                    message: "Este campo deve ter pelo menos 10 caracteres"
+                  },
+                  maxLength: {
+                    value: 30,
+                    message: "Este campo deve ter no máximo 30 caracteres"
+                  },
+                  pattern: {
+                    value: /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/gi,
+                    message: "Este email não é válido."
+                  }
+                }
+              )}
+            />
+            <span className='error'>{errors.email?.message}</span>
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            controlId="password"
+          >
+            <Form.Label>Senha</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="*********"
+              {...register("password",
+                {
+                  required: "Este campo é obrigatório.",
+                  minLength: {
+                    value: 8,
+                    message: "Este campo deve ter pelo menos 8 caracteres"
+                  },
+                  maxLength: {
+                    value: 12,
+                    message: "Este campo deve ter no máximo 12 caracteres"
+                  },
+                  pattern: {
+                    value: /(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$&]).+/gi,
+                    message: 'A sua senha de conter pelo menos  1 letra maiúscula letras minúsculas e pelo menos 1 caracter especial detre estes (@ # $ &)'
+                  }
+                }
+              )}
+            />
+            <span className='error'>{errors.password?.message}</span>
+          </Form.Group>
           <ButtonSubmit>
             Finalizar Cadastro
           </ButtonSubmit>
-        </form>
+        </Form>
       </div>
     </div>
   )
