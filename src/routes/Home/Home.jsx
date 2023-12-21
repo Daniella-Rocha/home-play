cdc2f78b64b09b55c9e72abd33c21122c75555ef
Daniella-Rocha <daniella.fp.dr@gmail.com>
2023-12-21 14:45:41 -0300

    Adicionado renderizacao condicioanl se tiver login

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,8 +1,14 @@
+import { useContext, useEffect } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
 import { PopularMoviesListContext } from '../../contexts/popularMoviesContext';
 
 import { TopRatedMoviesListContext } from '../../contexts/topRatedContext';
 
 import { NowPlayingData } from '../../contexts/nowPlayingContext';
+
+import { UserDataContext } from '../../contexts/userDataContext';
 
 import HomeBanner from '../../components/HomeBanner/HomeBanner';
 
@@ -16,40 +22,48 @@ import styles from './Home.module.css';
 
 const Home = () => {
 
+  const data = useContext(UserDataContext);
+
+  const navigate = useNavigate();
+
   const nowPlaying = useTmdbData(`/now_playing?api_key=${apiKey}&language=pt-BR`);
 
   const popular = useTmdbData(`/popular?api_key=${apiKey}&language=pt-BR`);
 
   const topRated = useTmdbData(`/top_rated?api_key=${apiKey}&language=pt-BR`);
 
-  return (
-    <div className={styles.home_container}>
-      <HomeBanner />
+  if (data.email && data.password) {
+    return (
+      <div className={styles.home_container}>
+        <HomeBanner />
 
-      <NowPlayingData.Provider value={nowPlaying}>
-        <Section
-          title={'Laçamentos'}
-          context={NowPlayingData}
+        <NowPlayingData.Provider value={nowPlaying}>
+          <Section
+            title={'Laçamentos'}
+            context={NowPlayingData}
 
-        />
-      </NowPlayingData.Provider>
+          />
+        </NowPlayingData.Provider>
 
-      <PopularMoviesListContext.Provider value={popular}>
-        <Section
-          title={'Populares'}
-          context={PopularMoviesListContext}
-        />
-      </PopularMoviesListContext.Provider>
+        <PopularMoviesListContext.Provider value={popular}>
+          <Section
+            title={'Populares'}
+            context={PopularMoviesListContext}
+          />
+        </PopularMoviesListContext.Provider>
 
-      <TopRatedMoviesListContext.Provider value={topRated}>
-        <Section
-          title={'Mais Bem Avaliados'}
-          context={TopRatedMoviesListContext}
-        />
-      </TopRatedMoviesListContext.Provider>
-    </div>
-
-  )
+        <TopRatedMoviesListContext.Provider value={topRated}>
+          <Section
+            title={'Mais Bem Avaliados'}
+            context={TopRatedMoviesListContext}
+          />
+        </TopRatedMoviesListContext.Provider>
+      </div>
+    )
+  }
+  useEffect(() => {
+    navigate('/');
+  }, []);
 }
 
 export default Home
