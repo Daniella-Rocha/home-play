f65d02eea7539a74435a46a3847941b5be71bb66
Daniella-Rocha <daniella.fp.dr@gmail.com>
2023-12-21 15:08:32 -0300

    Adicionado botao favorito

--- src/components/SlideItem/SlideItem.jsx
+++ src/components/SlideItem/SlideItem.jsx
@@ -1,79 +1,39 @@
-import { useState, useContext } from "react";
+import { useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
-import { UserFavoritesContext } from '../../contexts/useFavorites';
-
-import useFavorite from '../../hooks/useFavorites';
-
 import { LuMonitorPlay } from "react-icons/lu";
-
-import { FaRegHeart } from "react-icons/fa";
-
-import { FaHeart } from "react-icons/fa6";
 
 import styles from './SlideItem.module.css';
 
+import FavoriteButton from "../FavoriteButton/FavoriteButton";
+
 const SlideItem = ({ movie }) => {
 
-    const { title, backdrop_path, id } = movie;
+    const { id, poster_path } = movie;
 
-    const [isFav, setIsFav] = useState(false);
-
-    const [getFavoritesList] = useContext(UserFavoritesContext);
-
-    const { favList, setFavList, defineFavoritesList } = useFavorite();
-
-    const isFavorite = (movie) => {
-        setIsFav(!isFav);
-        if (!isFav) {
-            const favorite = favList.filter((fav, index, array) =>
-                array.indexOf(fav) !== index);
-            setFavList((prevState) => {
-                return prevState,
-                    { ...favorite }
-            });
-            defineFavoritesList();
-            
-        } else{
-            setIsFav(!isFav);
-        }
-
-    }
-
+    useEffect(() => {
+    }, [favList]);
 
     return (
         <div
-            className={styles.slide_cover}>
-            <img src={`https://image.tmdb.org/t/p/w300${backdrop_path}`} alt={`imagem da capa do filme${title}`} />
-
-            <div className={styles.slide_menu}
-                style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w300${backdrop_path}')` }}
-            >
-                <div>
-                    <h3>{title}</h3>
-                </div>
+            className={styles.slide_container}
+            style={{ backgroundImage: `url("https://image.tmdb.org/t/p/w300${poster_path}")` }}
+        >
+            <div className={styles.slide_menu}>
                 <div>
                     <button
                         type="button"
+                        className={styles.play}
+                        value={'assistir'}
                     >
                         <Link to={`/movie/${id}`}>
                             <LuMonitorPlay />
                         </Link>
                     </button>
-                    <button
-                        type="button"
-                        onClick={() => isFavorite(movie)}
-                    >
-                        {
-                            isFav ? <FaHeart
-                                className={`${isFav ? styles.fav : ''}`}
-                            />
-                                :
-                                <FaRegHeart />
-                        }
-
-                    </button>
+                </div>
+                <div>
+                    <FavoriteButton />
                 </div>
             </div>
         </div>
